2c258576dc3c85b43bd75a632c0db08df07941fa
a-mackay <amk@gegroup.com.au>
2019-03-19 17:09:10 +1100

    Complete exercises 1 and 3

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,41 +1,32 @@
 import Browser
+import Char exposing (isUpper, isLower, isDigit)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
-
-
--- MAIN
-
+import Html.Events exposing (onInput, onClick)
+import List exposing (any)
+import String exposing (length, toList, toInt)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
-
-
-
--- MODEL
-
 
 type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : String
   }
-
 
 init : Model
 init =
-  Model "" "" ""
-
-
-
--- UPDATE
+  Model "" "" "" ""
 
 
 type Msg
   = Name String
   | Password String
   | PasswordAgain String
+  | Age String
+  | DoNothing
 
 
 update : Msg -> Model -> Model
@@ -50,9 +41,10 @@ update msg model =
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Age age ->
+      { model | age = age }
 
-
--- VIEW
+    DoNothing -> model
 
 
 view : Model -> Html Msg
@@ -61,7 +53,9 @@ view model =
     [ viewInput "text" "Name" model.name Name
     , viewInput "password" "Password" model.password Password
     , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewInput "text" "Age in Years (an integer)" model.age Age
     , viewValidation model
+    , button [ onClick DoNothing ] [ text "Submit" ]
     ]
 
 
@@ -72,7 +66,28 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
+  if isModelValid model then
     div [ style "color" "green" ] [ text "OK" ]
   else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+    div [ style "color" "red" ] [ text "Model does not pass validation!" ]
+
+isModelValid : Model -> Bool
+isModelValid model =
+  let minimumNumberOfCharacters = 5
+      passwordsMatch = model.password == model.passwordAgain
+      passwordExceedsMinimumNumberOfCharacters = String.length model.password >= minimumNumberOfCharacters
+      passwordChars = String.toList model.password
+      containsUppercaseChar = List.any (\char -> Char.isUpper char) passwordChars
+      containsDigitChar = List.any (\char -> Char.isDigit char) passwordChars
+      containsLowercaseChar = List.any (\char -> Char.isLower char) passwordChars
+      ageIsAnInt = case String.toInt model.age of
+        Just _ -> True
+        Nothing -> False
+  in (
+    passwordsMatch
+    && passwordExceedsMinimumNumberOfCharacters
+    && ageIsAnInt
+    && containsUppercaseChar
+    && containsDigitChar
+    && containsLowercaseChar
+  )
